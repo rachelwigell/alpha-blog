e1737ccf610f104c9cf9ad73b714dfeadcce5a2a
Rachel Wigell <rwigell@patientslikeme.com>
2017-05-31 14:21:57 -0400

    enforce restrictions on the server side

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:edit, :update, :show]
+  before_action :require_same_user, only: [:edit, :update]
 
   def new
     @user = User.new
@@ -9,6 +10,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       flash[:success] = "Welcome to the alpha blog, #{@user.username}!"
+      session[:user_id] = @user.id
       redirect_to articles_path
     else
       render 'new'
@@ -43,5 +45,12 @@ class UsersController < ApplicationController
 
   def set_user
     @user = User.find(params[:id])
+  end
+
+  def require_same_user
+    unless logged_in? && current_user != @user
+      flash[:danger] = "You can only modify your own account"
+      redirect_to root_path
+    end
   end
 end
